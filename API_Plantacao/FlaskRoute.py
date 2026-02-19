c5cd2a28739c33644a75dbc17c0aa5dd3f07f4b2
Lucca Costa Lima D'Avila <luccacldavila@gmail.com>
2026-02-18 22:37:18 -0300

    Update FlaskRoute.py

--- API_Plantacao/FlaskRoute.py
+++ API_Plantacao/FlaskRoute.py
@@ -4,26 +4,20 @@ from flask import render_template
 app = Flask(__name__)
 sensores = []
 @app.route('/')
-def hello():
-    return 'TesteAPI'
-@app.route('/recebersensor', methods=['POST'])
-def recebersensor():
-    
+def principal():
+    return render_template('index.html')
+@app.route('/sensores', methods = ['GET'])
+def get_sensores():
+    return {"sensores": sensores}
+
+@app.route('/recebersensores', methods = ['POST'])
+def receber_sensores():
     data = request.get_json()
     sensores.append(data)
-    return {"message": "Sensor recebido com sucesso",
-        "total": len(sensores) }
-
-@app.route('/sensores', methods=['GET'])
-def get_sensores():
-    return sensores
-@app.route("/ultimosensor", methods=['GET'])
-def get_ultimo_sensor():
-    if sensores:
-        return sensores[-1]
+    return {"message": "Sensor recebido com sucesso!"}, 200
+@app.route('/selecionarsensor/<int:index>', methods = ['GET'])
+def selecionar_sensor(index):
+    if index < len(sensores):
+        return {"sensor": sensores[index]}, 200
     else:
-        return {"message": "Nenhum sensor recebido ainda."}
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+        return {"message": "Sensor não encontrado!"}, 404
